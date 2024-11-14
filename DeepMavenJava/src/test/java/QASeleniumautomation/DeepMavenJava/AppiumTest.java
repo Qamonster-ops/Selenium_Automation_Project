2d9dbd51510e5632a94df96a19c7feea9ad3f23d
21220795 <sweety.thota95@gmail.com>
2024-11-14 16:13:53 -0500

    Eleventh commit

---
+++ DeepMavenJava/src/test/java/QASeleniumautomation/DeepMavenJava/AppiumTest.java
@@ -0,0 +1,18 @@
+package QASeleniumautomation.DeepMavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("NativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+	}
+}
---
+++ DeepMavenJava/src/test/java/QASeleniumautomation/DeepMavenJava/RestAPITest.java
@@ -0,0 +1,19 @@
+package QASeleniumautomation.DeepMavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+//rahulonlinetutor@gmail.com
+public class RestAPITest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");	
+	}
+}
